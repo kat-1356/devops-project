a5823db787c157b8acbbb0df3e65d45dca8ee504
kat-1356 <khyatitrip@gmail.com>
2025-05-15 12:08:21 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+ // index.js
+
+// Simple echo function
+function echo(input) {
+  return input;
+}
+
+// Function to greet cake lovers
+function greet(name) {
+  return `Welcome to Cake Diaries, ${name}! 🎂`;
+}
+
+// Export functions for testing
+module.exports = { echo, greet };
